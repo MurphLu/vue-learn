0a935158eaf7c1d48eb19c68868c6833e0777a14
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2020-04-09 16:02:08 +0800

    add  bind

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <HelloWorld msg="Welcome to Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
@@ -22,6 +24,8 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
+}
+a {
+  color: #42b983;
 }
 </style>
---
+++ src/components/002.TwoWayBinding.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <label class="card-title">Form Demo</label>
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" v-model="user.name">
+      </div>
+      <div class="form-group">
+        <label for="age">age</label>
+        <input type="text" class="form-control" id="age" v-model="user.age">
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="0" v-model="user.gender">
+        <label class="form-check-label" for="inlineRadio1">male</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="1" v-model="user.gender">
+        <label class="form-check-label" for="inlineRadio2">female</label>
+      </div> <br>
+      <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1" v-model="user.vip">
+        <label class="form-check-label" for="exampleCheck1">isVIP</label>
+      </div>
+      <div class="form-group">
+        <label for="exampleFormControlSelect2">Example multiple select</label>
+        <select class="form-control" id="exampleFormControlSelect2" v-model="user.favoriteColor" @select="selectFavoriteColor">
+          <option disabled selected value="">请选择</option>
+          <option v-for="color in colors" :key="color" v-bind:value="color">{{ color }}</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import $ from 'jquery'
+export default {
+  
+  data() {
+    return {
+      user: {
+        name: "",
+        age:"",
+        gender: 0,
+        vip: false,
+        favoriteColor:"",
+      },
+      colors: [
+        "red", "blue", "yellow", "green", "dark"
+      ]
+    }
+  },
+  methods: {
+    selectFavoriteColor() {
+      console.log(this.user.favoriteColor)
+      $("#exampleFormControlSelect2").css("background-color", this.user.favoriteColor)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

446a137c459b6503bf29c3431ea1525c1bcbe0b4
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2020-05-15 17:52:14 +0800

    add nav bar

--- 02_compoents_communication/src/App.vue
+++ 02_compoents_communication/src/App.vue
@@ -1,24 +1,21 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">vue</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarText">
-        <div class="navbar-nav">
-          <router-link to="/home" class='nav-item nav-link' activeClass='active' exactActiveClass=''>Home</router-link>
-          <router-link to="/about" class='nav-item nav-link' activeClass='active' exactActiveClass=''>About</router-link>
-        </div>
-      </div>
-    </nav>
-    <!-- <div id="nav"> -->
-      <!-- <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> -->
-    <!-- </div> -->
+    <NavBar />
     <router-view/>
   </div>
 </template>
 
+<script>
+import NavBar from './components/NavBar.vue'
+export default {
+  components: {
+    NavBar,
+  },
+}
+</script>
+
+<style>
+
+</style>
 <style>
 </style>
---
+++ 02_compoents_communication/src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">vue</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <div class="navbar-nav">
+          <router-link to="/home" class='nav-item nav-link' activeClass='active' exactActiveClass=''>Home</router-link>
+          <router-link to="/about" class='nav-item nav-link' activeClass='active' exactActiveClass=''>About</router-link>
+        </div>
+      </div>
+    </nav>
+</template>
+
+<script>
+
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
